7d39ccb6b9d1853800fd2fd34e79624631b4720a
luciano393 <luciano39340268@gmail.com>
2021-09-26 02:36:16 -0300

    Update LoginScreen.tsx

--- src/pages/Auth/LoginScreen.tsx
+++ src/pages/Auth/LoginScreen.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { IoLogoGithub, IoLogoLinkedin, IoLogoGoogle } from 'react-icons/io5'
+
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 
@@ -7,6 +9,8 @@ import { InputText } from '../../components/Input/InputText';
 import { IFormSubmitLogin } from '../../interfaces/IFormSubmit';
 
 import { FormSchema } from '../../validators/FormSchema';
+import { ButtonComponent } from '../../components/ButtonComponent/ButtonComponent';
+import { Title } from '../../components/Title/Title';
 
 export const LoginScreen = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<IFormSubmitLogin>({
@@ -18,11 +22,24 @@ export const LoginScreen = () => {
     }
 
     return (
-        <div>
+        <div className="box-container">
+            <header>
+                <Title text="Hackathon Gentleman Programming 2021"/>
+            </header>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <InputText placeholder="example@gmail.com" label="Email" register={register("email")} error={errors?.email} />
                 <InputText placeholder="******" label="Contraseña" type="password" register={register("password")} error={errors?.password} />
-                <button type="submit">Loguearse</button>
+                <a href="#">¿Olvidaste tu contraseña?</a>
+                <div>
+                    <IoLogoGithub className="icon"/>
+                    <IoLogoGoogle className="icon"/>
+                    <IoLogoLinkedin className="icon"/>
+                </div>
+                <ButtonComponent text="Login" type="submit"/>
+                <div className="registrer">
+                    <a href="#">Registrar usuario</a>
+                    <a href="#">Registrar empresa</a>
+                </div>
             </form>
         </div>
     )
