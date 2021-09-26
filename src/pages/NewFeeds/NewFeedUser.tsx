9b65a21e589a8810f41c75342f89f62bb5a4556f
luciano393 <luciano39340268@gmail.com>
2021-09-26 17:03:04 -0300

    Create NewFeedUser.tsx

---
+++ src/pages/NewFeeds/NewFeedUser.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ButtonComponent } from '../../components/ButtonComponent/ButtonComponent'
+import { FeedComponentUser } from '../../components/FeedComponent/FeedComponent'
+import { Title, Titlesmall } from '../../components/Title/Title'
+
+export const NewFeedUser = () => {
+    return (
+        <div className="container-feed">
+            <header>
+                <div className="titles">
+                    <Title text="Hackathon Gentleman Programming 2021" />
+                    <Titlesmall text="Bootcamps" />
+                </div>
+                <div className="button-header">
+                     <ButtonComponent text="Cerrrar sesion" color="btn secondary"/>
+                </div>
+            </header>
+            <section className="bootcamps">
+                <FeedComponentUser/>
+            </section>
+        </div>
+    )
+}
