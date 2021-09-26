83aefbebb0871de5b9f2e6de19e40d6f9c5e281a
Alex <wilmer.delgadoalama@gmail.com>
2021-09-26 05:44:11 -0500

    add message error

--- src/utils/MessageError.ts
+++ src/utils/MessageError.ts
@@ -3,6 +3,20 @@ class MessageError{
     public static readonly EMAIL_REQUIRED = "El correo es requerido";
 
     public static readonly PASSWORD_REQUIRED = "La contraseña es requerida";
+
+    public static readonly NAME_REQUIRED = "El nombre es requerido";
+
+    public static readonly LINKEDIN_REQUIRED = "El link de linkedin es requerido";
+
+    public static readonly COUNTRY_REQUIRED = "El país es requerido";
+
+    public static readonly REPOSITORY_REQUIRED = "El repositorio es requerido";
+
+    public static readonly PASSWORD_IS_NOT_MATCH = "Las contraseñas no coinciden";
+
+    public static readonly DESCRIPTION_REQUIRED = "La descripción es requerida";
+
+    public static readonly WEBSITE_REQUIRED = "El sitio web es requerido";
 }
 
 export { MessageError };
--- src/validators/FormSchema.ts
+++ src/validators/FormSchema.ts
@@ -10,27 +10,27 @@ class FormSchema {
     });
 
     public static readonly register: any = yup.object({
-        name: yup.string().required(),
-        linkedIn: yup.string().required(),
-        country: yup.string().required(),
-        repository: yup.string().required(),
+        name: yup.string().required(MessageError.NAME_REQUIRED),
+        linkedIn: yup.string().required(MessageError.LINKEDIN_REQUIRED),
+        country: yup.string().required(MessageError.COUNTRY_REQUIRED),
+        repository: yup.string().required(MessageError.REPOSITORY_REQUIRED),
         password: yup.string().required(MessageError.PASSWORD_REQUIRED),
         repeatPassword: yup.string()
-            .oneOf([yup.ref('password')], 'Passwords must match')
+            .oneOf([yup.ref('password')], MessageError.PASSWORD_IS_NOT_MATCH)
             .required(MessageError.PASSWORD_REQUIRED),
-        description: yup.string().required(),
+        description: yup.string().required(MessageError.DESCRIPTION_REQUIRED),
     });
 
     public static readonly registerCompany: any = yup.object({
-        name: yup.string().required(),
-        linkedIn: yup.string().required(),
-        country: yup.string().required(),
+        name: yup.string().required(MessageError.NAME_REQUIRED),
+        linkedIn: yup.string().required(MessageError.LINKEDIN_REQUIRED),
+        country: yup.string().required(MessageError.COUNTRY_REQUIRED),
         website: yup.string().required(),
         password: yup.string().required(MessageError.PASSWORD_REQUIRED),
         repeatPassword: yup.string()
-            .oneOf([yup.ref('password')], 'Passwords must match')
+            .oneOf([yup.ref('password')], MessageError.PASSWORD_IS_NOT_MATCH)
             .required(MessageError.PASSWORD_REQUIRED),
-        description: yup.string().required(),
+        description: yup.string().required(MessageError.DESCRIPTION_REQUIRED),
     });
 
 }
