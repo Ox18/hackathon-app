8988ed509194e668c39d3222e88a3f8e3133fe5f
luciano393 <luciano39340268@gmail.com>
2021-09-26 18:21:59 -0300

    Update NotFounScreen.tsx

--- src/pages/NotFound/NotFounScreen.tsx
+++ src/pages/NotFound/NotFounScreen.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ButtonComponent } from '../../components/ButtonComponent/ButtonComponent'
 
 export const NotFounScreen = () => {
     return (
-        <div>
-            No se encontro lo que buscabas
+        <div className="not-found">
+            <div className="not-found__content">
+                <h1>404</h1>
+                <h5>Ops! Page not found :(</h5>
+                <ButtonComponent text="Back to Home" color="btn primary"/>
+            </div>
         </div>
     )
 }
