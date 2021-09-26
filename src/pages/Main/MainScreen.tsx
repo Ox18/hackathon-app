43a5cf7596e1df5fd72f7cd782e701210f568b9e
luciano393 <luciano39340268@gmail.com>
2021-09-26 02:36:32 -0300

    Update MainScreen.tsx

--- src/pages/Main/MainScreen.tsx
+++ src/pages/Main/MainScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { LoginScreen } from '../Auth/LoginScreen'
 
 export const MainScreen = () => {
     return (
         <div>
-            Hello world!
+            <LoginScreen />
         </div>
     )
 }
